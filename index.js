d205eddc60153fa84cdcd250cd855dd8c95ec803
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2022-12-28 11:43:14 +0100

    Final

--- index.js
+++ index.js
@@ -1,34 +1,85 @@
+var currentCharacter = "O"
+
+var board = [
+    ["/", "/", "/"],
+    ["/", "/", "/"],
+    ["/", "/", "/"]
+]
+
 function buildTable(){
     document.getElementById("tictactoe").innerHTML = /*html*/ `
     <tbody>
         <tr>
-            <td><button id="btn00"></button></td>
-            <td><button id="btn10"></button></td>
-            <td><button id="btn20"></button></td>
+            <td style="border-top: 0; border-left: 0"> <button id="btn00"></button></td>
+            <td style="border-top: 0;">                <button id="btn01"></button></td>
+            <td style="border-top: 0; border-right: 0"><button id="btn02"></button></td>
         </tr>
         <tr>
-            <td><button id="btn01"></button></td>
-            <td><button id="btn11"></button></td>
-            <td><button id="btn21"></button></td>
+            <td style="border-left: 0"> <button id="btn10"></button></td>
+            <td>                        <button id="btn11"></button></td>
+            <td style="border-right: 0"><button id="btn12"></button></td>
         </tr>
         <tr>
-            <td><button id="btn02"></button></td>
-            <td><button id="btn12"></button></td>
-            <td><button id="btn22"></button></td>
+            <td style="border-bottom: 0; border-left: 0"> <button id="btn20"></button></td>
+            <td style="border-bottom: 0;">                <button id="btn21"></button></td>
+            <td style="border-bottom: 0; border-right: 0"><button id="btn22"></button></td>
         </tr>
     </tbody>`
 }
 
-function onButtonClick(event){
-    // Tabellenzelle bestimmen
-			var td = event.target;
+function announceWin(char){
+    let winString = char === "X" ? "<span style=\"color: #4D00FF\">X</span>" : "<span style=\"color: #ff004D\">O</span>"
+    document.body.innerHTML += "<h1 class=\"win\">Spieler '"+winString+"' hat gewonnen</h1>"
+    document.getElementById("tictactoe").classList.add("slideOut")
+}
 
-			// Button oder Zelle?
-			while (td.tagName.toLowerCase() != "td"
-				&& td != field
-			) {
-				td = td.parentNode;
-			}
+function announceTie(){
+    document.body.innerHTML += "<h1 class=\"win\">Unentschieden</h1>"
+    document.getElementById("tictactoe").classList.add("slideOut")
+}
+
+function checkForWins(){
+    // Vertikal: X = 0, X = 1, X = 2
+    if(board[0][0] === board[1][0] && board[1][0] === board[2][0] && board[0][0] !== "/"){ announceWin(board[0][0]); return }
+    if(board[0][1] === board[1][1] && board[1][1] === board[2][1] && board[0][1] !== "/"){ announceWin(board[0][1]); return }
+    if(board[0][2] === board[1][2] && board[1][2] === board[2][2] && board[0][2] !== "/"){ announceWin(board[0][2]); return }
+
+    // Horizontal: Y = 0, Y = 1, Y = 2
+    if(board[0][0] === board[0][1] && board[0][1] === board[0][2] && board[0][0] !== "/"){ announceWin(board[0][0]); return }
+    if(board[1][0] === board[1][1] && board[1][1] === board[1][2] && board[1][0] !== "/"){ announceWin(board[1][0]); return }
+    if(board[2][0] === board[2][1] && board[2][1] === board[2][2] && board[2][0] !== "/"){ announceWin(board[2][0]); return }
+
+    // Diagonal
+    if(board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[1][1] !== "/"){ announceWin(board[1][1]); return }
+    if(board[2][0] === board[1][1] && board[1][1] === board[0][2] && board[1][1] !== "/"){ announceWin(board[1][1]); return }
+
+    // Unentschieden
+    for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 3; y++) {
+            if(board[y][x] === "/") return
+        }
+    }
+    announceTie()
+}
+
+function onButtonClick(event){
+    var td = event.target;
+    if(td.tagName !== "TD") return
+
+    let id = event.target.getElementsByTagName("button")[0].id
+    let x = parseInt(id[4])
+    let y = parseInt(id[3])
+    board[y][x] = currentCharacter
+
+    if(currentCharacter === "O"){
+        td.innerHTML = "<p style=\"--color: #ff004D\">O</p>"
+    }else{
+        td.innerHTML = "<p style=\"--color: #4D00FF\">X</p>"
+    }
+
+    currentCharacter = currentCharacter === "O" ? "X" : "O"
+
+    checkForWins();
 }
 
 document.addEventListener("DOMContentLoaded", buildTable)
