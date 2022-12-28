46b205def7d4202a2672b1dfc74c49f06d1cfd7d
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2022-12-28 11:44:36 +0100

    Format

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var board = [
     ["/", "/", "/"]
 ]
 
-function buildTable(){
+function buildTable() {
     document.getElementById("tictactoe").innerHTML = /*html*/ `
     <tbody>
         <tr>
@@ -27,53 +27,53 @@ function buildTable(){
     </tbody>`
 }
 
-function announceWin(char){
+function announceWin(char) {
     let winString = char === "X" ? "<span style=\"color: #4D00FF\">X</span>" : "<span style=\"color: #ff004D\">O</span>"
-    document.body.innerHTML += "<h1 class=\"win\">Spieler '"+winString+"' hat gewonnen</h1>"
+    document.body.innerHTML += "<h1 class=\"win\">Spieler '" + winString + "' hat gewonnen</h1>"
     document.getElementById("tictactoe").classList.add("slideOut")
 }
 
-function announceTie(){
+function announceTie() {
     document.body.innerHTML += "<h1 class=\"win\">Unentschieden</h1>"
     document.getElementById("tictactoe").classList.add("slideOut")
 }
 
-function checkForWins(){
+function checkForWins() {
     // Vertikal: X = 0, X = 1, X = 2
-    if(board[0][0] === board[1][0] && board[1][0] === board[2][0] && board[0][0] !== "/"){ announceWin(board[0][0]); return }
-    if(board[0][1] === board[1][1] && board[1][1] === board[2][1] && board[0][1] !== "/"){ announceWin(board[0][1]); return }
-    if(board[0][2] === board[1][2] && board[1][2] === board[2][2] && board[0][2] !== "/"){ announceWin(board[0][2]); return }
+    if (board[0][0] === board[1][0] && board[1][0] === board[2][0] && board[0][0] !== "/") { announceWin(board[0][0]); return }
+    if (board[0][1] === board[1][1] && board[1][1] === board[2][1] && board[0][1] !== "/") { announceWin(board[0][1]); return }
+    if (board[0][2] === board[1][2] && board[1][2] === board[2][2] && board[0][2] !== "/") { announceWin(board[0][2]); return }
 
     // Horizontal: Y = 0, Y = 1, Y = 2
-    if(board[0][0] === board[0][1] && board[0][1] === board[0][2] && board[0][0] !== "/"){ announceWin(board[0][0]); return }
-    if(board[1][0] === board[1][1] && board[1][1] === board[1][2] && board[1][0] !== "/"){ announceWin(board[1][0]); return }
-    if(board[2][0] === board[2][1] && board[2][1] === board[2][2] && board[2][0] !== "/"){ announceWin(board[2][0]); return }
+    if (board[0][0] === board[0][1] && board[0][1] === board[0][2] && board[0][0] !== "/") { announceWin(board[0][0]); return }
+    if (board[1][0] === board[1][1] && board[1][1] === board[1][2] && board[1][0] !== "/") { announceWin(board[1][0]); return }
+    if (board[2][0] === board[2][1] && board[2][1] === board[2][2] && board[2][0] !== "/") { announceWin(board[2][0]); return }
 
     // Diagonal
-    if(board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[1][1] !== "/"){ announceWin(board[1][1]); return }
-    if(board[2][0] === board[1][1] && board[1][1] === board[0][2] && board[1][1] !== "/"){ announceWin(board[1][1]); return }
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[1][1] !== "/") { announceWin(board[1][1]); return }
+    if (board[2][0] === board[1][1] && board[1][1] === board[0][2] && board[1][1] !== "/") { announceWin(board[1][1]); return }
 
     // Unentschieden
     for (let x = 0; x < 3; x++) {
         for (let y = 0; y < 3; y++) {
-            if(board[y][x] === "/") return
+            if (board[y][x] === "/") return
         }
     }
     announceTie()
 }
 
-function onButtonClick(event){
+function onButtonClick(event) {
     var td = event.target;
-    if(td.tagName !== "TD") return
+    if (td.tagName !== "TD") return
 
     let id = event.target.getElementsByTagName("button")[0].id
     let x = parseInt(id[4])
     let y = parseInt(id[3])
     board[y][x] = currentCharacter
 
-    if(currentCharacter === "O"){
+    if (currentCharacter === "O") {
         td.innerHTML = "<p style=\"--color: #ff004D\">O</p>"
-    }else{
+    } else {
         td.innerHTML = "<p style=\"--color: #4D00FF\">X</p>"
     }
 
